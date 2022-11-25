797bd57f29edb951f3737406dd8204d2cb43fb01
Abhinaireddy-Duba <abhinaireddy2002@gmail.com>
2022-11-25 08:32:26 +0530

    post action

---
+++ index.js
@@ -0,0 +1,28 @@
+const express=require("express")
+const app=express()
+const port=2002;
+const cors=require("cors")
+const bodyParser=require("body-parser")
+
+app.use(bodyParser.urlencoded({
+	extended:true
+
+
+
+}))
+app.use(bodyParser.json())
+app.use(cors())
+
+
+
+
+
+app.get("/",(req,res)=>{
+	res.send("server is created")
+})
+app.post("/newData",(req,res)=>{
+	console.log(req.body)
+	/*const {name,age}=req.body
+	console.log(name,age,"values")*/
+})
+app.listen(port)
